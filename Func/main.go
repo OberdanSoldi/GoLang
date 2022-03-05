5c9d6ee2c4dc2f22618187a7638723912f22220e
oberdansoldi <oberdan_soldi@outlook.com.br>
2022-03-04 23:30:46 -0300

    Func

--- Func/main.go
+++ Func/main.go
@@ -9,8 +9,23 @@ func add(x int, y int) int { // ou pode ser declarado como (x, y int)
 	return x + y
 }
 
+func swap(x, y string) (string, string) { //A função pode retornar qualquer número de dados
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 1
+	y = sum - x
+	return
+}
+
 func main() {
 	fmt.Println("Soma 1 + 1 =", add(1, 1))
 	fmt.Println("Soma 3 + 3 =", add(3, 3))
 	fmt.Println("Soma 4 + 4 =", add(4, 4))
+	//-------------------------------------//
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	//-------------------------------------//
+	fmt.Println(split(17))
 }
